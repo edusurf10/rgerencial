e33c22667be7cc76a4f7d7e2b074cff9f7380b46
Luiz Eduardo de Goes Maciel <tutoriais.gamehelp@gmail.com>
2022-01-02 04:35:53 -0300

    Finished

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+    @users = User.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, controllers: { registrations: 'user/registrations' }
+  resources :users
+  devise_for :admins
+  resources :admins
+  get 'home/index'
+  root to: "home#index"
 end
